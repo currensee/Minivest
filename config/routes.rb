ea81e21799e37d4e0716fa80d391f35d9ed1640c
venturemedias <elliot@venturemedias.com>
2011-03-03 15:58:44 -0500

    root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Minivest::Application.routes.draw do
   devise_for :users
 
   resources :trade_leaders
+  
+  root :to => "welcome#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
